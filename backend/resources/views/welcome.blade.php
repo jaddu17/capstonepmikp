56669e563977679eb421ace3d23e6b360911e150
jaddu17 <shafajaddu@gmail.com>
2026-06-09 23:53:42 +0700

    9/6/2026

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Website Design for PMI Kulon Progo</title>
     <style>html, body { height: 100%; margin: 0; } #root { height: 100%; }</style>
-    @viteReactRefresh
-    @vite('src/main.tsx')
   </head>
   <body>
     <div id="root"></div>
