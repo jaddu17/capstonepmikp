c7c972c5ad519e6b44d235be0dda18a2b4ea0820
LindaPermata <permatalinda531@gmail.com>
2026-06-28 12:52:34 +0700

    28/6/26

---
+++ storage/framework/views/2b1edbe02bc34e5aa27cfd587f6a2e8b.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Website Design for PMI Kulon Progo</title>
+    <style>html, body { height: 100%; margin: 0; } #root { height: 100%; }</style>
+    <?php echo app('Illuminate\Foundation\Vite')->reactRefresh(); ?>
+    <?php echo app('Illuminate\Foundation\Vite')('src/main.tsx'); ?>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
+<?php /**PATH D:\SMT6\capstoneprojekpmi\capstonepmikp\resources\views/welcome.blade.php ENDPATH**/ ?>
